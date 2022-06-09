028a324bb3e453eb9a4d3c65a5265e3104e9994d
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-06-09 17:06:56 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Code your solution in this file!
+
 const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 
 function returnFirstTwoDrivers() {
@@ -8,19 +8,25 @@ function returnFirstTwoDrivers() {
 function returnLastTwoDrivers(){
     return ['Amari', 'Mo']
 }
+
+// arrow function
+// const add = [parameter1 + parameter2]=>parameter1 + parameter2
 const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
 
-const createFareMultiplier = function (fareMultiplier) {
+
+//should multiply a given value using the created multiplier
+const createFareMultiplier = function (fareMultiplier){
   return function(value) {
-    return fareMultiplier * value;
-  }
-}
+        return fareMultiplier * value;
+      }
+    }   
+
 
 const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 
-
+//returns the first two drivers when passed returnFirstTwoDrivers() as the second argument
 function selectDifferentDrivers(drivers, func){
     return func(drivers);
 
